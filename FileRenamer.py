27fdafc2573ec71d8bcc7fce86633acb41b2705e
Márk Göbölös <44113348+gmark11@users.noreply.github.com>
2019-12-30 16:08:56 +0100

    Automatically detects the type of the files

--- FileRenamer.py
+++ FileRenamer.py
@@ -1,12 +1,13 @@
 import os
 
-folder = input("Write in the path of the folder where you want to rename the files: ")
-whatkind = input("What kind of file is this: ")
-
+folder = input(
+    "Write in the path of the folder where you want to rename the files: ")
 
 num = 1
-
 for file in os.listdir(folder):
-    name = "file" + str(num) + "." + whatkind
+    x, whatkind = os.path.splitext(file)
+    name = "file" + str(num) + whatkind  # the name of the new files
+
     os.rename(os.path.join(folder, file), os.path.join(folder, name))
+
     num = num + 1
